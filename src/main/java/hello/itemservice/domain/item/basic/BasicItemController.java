c651b7ec2fb0f8419a0dcb2e4396db778dbd3183
DevKs-Yet <ksang0324@gmail.com>
2022-03-08 13:33:01 +0900

    20220308 - 상품 목록 - 타임리프

---
+++ src/main/java/hello/itemservice/domain/item/basic/BasicItemController.java
@@ -0,0 +1,38 @@
+package hello.itemservice.domain.item.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        log.info("BasicItemController items - {}", model);
+        List<Item> items = itemRepository.findAll();
+        return "basic/items";
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("testA", 10000, 10));
+        itemRepository.save(new Item("testB", 20000, 20));
+    }
+
+}
